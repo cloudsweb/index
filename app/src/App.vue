8ba4c15ebc196dee7664f8e4be52bb5f4536fc0b
Clouds Flowing <clouds.flowing@gmail.com>
2020-05-09 13:21:06 +0800

    add logo

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">
+        <img alt="Axiom" src="./assets/logo.svg">
+      </router-link> |
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
