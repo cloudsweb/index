de80f3b4f3031c8801be0df22fd542f03b37d3d5
Clouds Flowing <clouds.flowing@gmail.com>
2020-05-09 12:30:04 +0800

    add topic

---
+++ app/src/components/Topic.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="hello">
+    <h1>{{ topicDisplay }}</h1>
+    <div class="loading" v-if="loading">
+      loading...
+    </div>
+    <ul class="topics" v-if="!loading">
+      <li v-for="sub in subtopics" :key="sub">{{ sub }}</li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() private topicId!: string;
+  loading = true;
+  subtopics: string[] | null = null;
+
+  get topicDisplay () {
+    return this.topicId
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Topic topic-id="~root"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Topic from '@/components/Topic.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Topic
   }
 }
 </script>
